b74eeeaecdf158187aeb3c3ce9d44d3ed3029f6b
Lucas Rezende <lucas.rezende.2000@gmail.com>
2020-06-18 15:06:58 -0300

    game logic

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
+let direction = "right";
 
 snake[0] = {
     x: 8 * box,
@@ -20,4 +21,40 @@ function createSnake(){
     }
 }
 
-criarBG();
+document.addEventListener('keydown', update);
+
+function updateMove(e){
+    if(e.keyCode == 37 && direction != "right") direction = "left";
+    if(e.keyCode == 38 && direction != "down") direction = "up";
+    if(e.keyCode == 39 && direction != "left") direction = "right";
+    if(e.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function game(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    criarBG();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let beginGame = setInterval(game, 100);
